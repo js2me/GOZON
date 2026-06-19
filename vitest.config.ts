461a9a25b95c402da39e6fc1df5e925879e56812
Sergey Volkov <js2me@outlook.com>
2026-06-20 00:51:56 +0300

    chore: deps update

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,13 @@ import { mergeConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(async (env) => {
+  const baseConfig =
+    typeof viteConfig === 'function' ? await viteConfig(env) : viteConfig;
+
+  return mergeConfig(baseConfig, {
     test: {
       environment: 'jsdom',
     },
-  }),
-);
+  });
+});
